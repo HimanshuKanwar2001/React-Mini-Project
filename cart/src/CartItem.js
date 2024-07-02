a29290a65b4538412e7b907432acb9d68f17a980
HimanshuKanwar2001 <himanshukanwar2001@gmail.com>
2024-07-02 09:23:04 +0530

    added state for the caritems

--- cart/src/CartItem.js
+++ cart/src/CartItem.js
@@ -1,17 +1,32 @@
 import React from "react";
 
 class CartItem extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            price:999,
+            title:'Phone',
+            qty:1,
+            img:''
+        }
+    }
   render() {
+    const {price,title,qty}=this.state;
     return (
       <div className="cart-item">
         <div className="left-block">
           <img style={styles.image} alt="" />
         </div>
         <div className="right-block">
-          <div style={{ fontSize: 25 }}>Phone</div>
-          <div style={{ color: "#777" }}>Rs 999</div>
-          <div style={{ color: "#777" }}>Qty:1</div>
-          <div className="car-item-actions">{/* { Button} */}</div>
+          <div style={{ fontSize: 25 }}>{title}</div>
+          <div style={{ color: "#777" }}>Rs {price}</div>
+          <div style={{ color: "#777" }}>Qty:{qty}</div>
+          <div className="car-item-actions">{/* { Button} */}
+            <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992651.png" />
+            <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png" />
+            <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/1214/1214428.png" />
+
+          </div>
         </div>
       </div>
     );
